952216b96d61e09354e42a3d461f9f03d7726e25
Efrén Ruíz <eruiz@getinsoft.com>
2022-09-13 11:42:24 -0500

    :sparkles: [ADD]: Data Annotations added

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectEF.Models;
 
 public class Category
 {
+    [Key]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(150)]
     public string? Name { get; set; }
     public string? Description { get; set; }
     public virtual ICollection<Work>? Works { get; set; }
--- Models/Work.cs
+++ Models/Work.cs
@@ -1,14 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace projectEF.Models;
 
 public class Work
 {
+    [Key]
     public Guid WorkId { get; set; }
+
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(200)]
     public string? Title { get; set; }
+
     public string? Description { get; set; }
+
     public Priority WorkPriority { get; set; }
+
     public DateTime CreatedAt { get; set; }
+
     public virtual Category? Category { get; set; }
+
+    [NotMapped]
+    public string? Summary { get; set; }
 }
 
 public enum Priority
